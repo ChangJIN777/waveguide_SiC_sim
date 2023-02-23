b4008db786cbba1aa5d461c961aa09e1b944568b
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-02-23 16:21:48 -0500

    debugging

--- run_AMD_SiC_Sweeps.sh
+++ run_AMD_SiC_Sweeps.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-#SBATCH -c 10                # Number of cores (-c)
-#SBATCH -t 0-00:30          # Runtime in D-HH:MM, minimum of 10 minutes
-#SBATCH -p serial_requeue   # Partition to submit to
-#SBATCH --mem=100           # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -o myoutput_%j.out  # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e myerrors_%j.err  # File to which STDERR will be written, %j inserts jobid
-module load Anaconda3/5.3.0 #Load Anaconda module
+module load Anaconda3 #Load Anaconda module
+source activate wv_sol_env # activate the wavesolver environment
+module load intel/21.2.0-fasrc01
+module load openmpi/4.1.1-fasrc01
 module load lumerical-seas/2021_7bf43e7149-fasrc01
 python3 AMD_SiNB_Sweeps.py
