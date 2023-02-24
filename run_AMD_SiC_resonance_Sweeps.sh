c9222609f4adfc02ac51515eb7701c1ab44f124b
Chang Jin <cjin@holy7c24104.rc.fas.harvard.edu>
2023-02-24 01:32:36 -0500

    debugging

--- run_AMD_SiC_resonance_Sweeps.sh
+++ run_AMD_SiC_resonance_Sweeps.sh
@@ -4,4 +4,4 @@ source activate wv_sol_env # activate the wavesolver environment
 module load intel/21.2.0-fasrc01
 module load openmpi/4.1.1-fasrc01
 module load lumerical-seas/2021_7bf43e7149-fasrc01
-python ./run_AMD_SiC_resonance_Sweeps.sh
+python AMD_SiC_resonance_Sweeps.py
