4e9477e9024ea08cdc948f5cc42043322cba7a95
Chang Jin <cjin@holy7c24102.rc.fas.harvard.edu>
2023-02-28 12:58:24 -0500

    debugging

--- AMD_SiC_Sweeps.py
+++ AMD_SiC_Sweeps.py
@@ -140,8 +140,8 @@ def fitness(params):
 	# writing the data into a csv file instead of a txt file for easier data analysis 
 	with open("OptimizeListFull_waveguide_parameters.csv","wb") as file_csv:
 		writer = csv.writer(file_csv, delimiter="\t")
-		params_bytes = bytes()
-		params_bytes = params_bytes.join((struct.pack('f',val) for val in params))
+        params_bytes = []
+        for i in params: params_bytes.append(bytes(i))
 		writer.writerow(params_bytes)
 	with open("OptimizeListFull_waveguide_char.csv","wb") as file_csv:
 		writer = csv.writer(file_csv, delimiter="\t")
