e0f59ef9056b4679549967c6688b0cd611ecdb08
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-01 12:51:36 -0500

    testing

--- AMD_SiC_resonance_Sweeps.py
+++ AMD_SiC_resonance_Sweeps.py
@@ -116,7 +116,7 @@ def fitness(params):
     cavity.save("cavity.obj")
 
     #define mesh size (use 10nm for accuracy, currently set to 12nm)
-    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 10e-9, dy=None, dz=None)
+    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 20e-9, dy=None, dz=None)
 
     r1 = cavity.simulate("resonance", target_freq=target_frequency, mesh_regions = [man_mesh], sim_size=Vec3(4,4,4))
 
