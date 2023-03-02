8b9948cb28820789a39bb47ed296563da89f6b06
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-02 13:42:24 -0500

    debugging

--- AMD_SiC_waveguide_test_Sweep_v1.py
+++ AMD_SiC_waveguide_test_Sweep_v1.py
@@ -167,7 +167,7 @@ def runSim(params):
     # r2.show()
     
     # writing the data into a csv file instead of a txt file for easier data analysis 
-    with open("./sim_data/OptimizeListFull_with_waveguide_test_sweep_t3.csv","a") as file_csv:
+    with open("./sim_data/OptimizeListFull_with_waveguide_test_sweep_t4.csv","a") as file_csv:
         writer = csv.writer(file_csv, delimiter="\t")
         writer.writerow([prefactor_mirror_R,t_wvg,Q,Qsc,Qwvg,Vmode,F,detuning_wavelength,fitness])
 
