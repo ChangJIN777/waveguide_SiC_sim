46425ff120835a39c6ce16a9e06bc832eacf0707
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-02 12:45:08 -0500

    added timer

--- AMD_SiC_waveguide_test_Sweep_v1.py
+++ AMD_SiC_waveguide_test_Sweep_v1.py
@@ -13,10 +13,12 @@ import numpy as np
 import os
 import scipy.constants
 import csv 
+from datetime import datetime
 
 # in this script, we will use a for loop to iterate through different number of unit cells in the weak 
 # mirror region and the waveguide region and calculate the properties of the resulting cavities 
 def runSim(params):
+    start_time = datetime.now()
     # Define geometry paramaters 
     #taper cell number (left mirror region)
     TN = 8
@@ -165,10 +167,13 @@ def runSim(params):
     # r2.show()
     
     # writing the data into a csv file instead of a txt file for easier data analysis 
-    with open("./sim_data/OptimizeListFull_with_waveguide_test_sweep_t2.csv","a") as file_csv:
+    with open("./sim_data/OptimizeListFull_with_waveguide_test_sweep_t3.csv","a") as file_csv:
         writer = csv.writer(file_csv, delimiter="\t")
         writer.writerow([prefactor_mirror_R,t_wvg,Q,Qsc,Qwvg,Vmode,F,detuning_wavelength,fitness])
-  
+
+    end_time = datetime.now()
+    print('Duration: {}'.format(end_time - start_time))
+    
     return -1*fitness
 
 
