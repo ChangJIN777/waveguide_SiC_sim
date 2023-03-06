db8d43d73bc0a5669a2eadea56d01c304255ff78
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-05 23:25:25 -0800

    testing the code

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -30,9 +30,9 @@ d = 0.64
 #beam width prefactor
 w = 1.75
 #taper prefactor (for the defect region)
-t = 0.84
+t = 0.5
 #taper prefactor (for the waveguide region)
-t_wvg = 0.84
+t_wvg = 0.5
 #beam height (set by epi-layer thickness)
 h0 = 250e-9
 # cavity beam length
@@ -91,7 +91,7 @@ while i <= TN:
     i = i+1 
 
 #set the center of the device 
-centerCell = MN_L+TN
+centerCell = MN_L+TN-1
 # centerCell = 0 # testing the code
 
 # adding waveguide region to the cavity 
