8de86135cf3210b6b11ce8c33fa97fe1e49a9847
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-06 22:58:03 -0800

    debugging

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -116,7 +116,9 @@ cavity.save("cavity_testing.obj")
 man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 20e-9, dy=None, dz=None)
 
 # simulating the resonance and the Q =================================================
-r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
+# r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
+#                     analyze_time=1000e-15,mesh_regions = [man_mesh], sim_size=Vec3(4,4,10))
+r1 = cavity.simulate("resonance", target_freq=target_frequency, 
                     analyze_time=1000e-15,mesh_regions = [man_mesh], sim_size=Vec3(4,4,10))
 
 # Print the reults and plot the electric field profiles
