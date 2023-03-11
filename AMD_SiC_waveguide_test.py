239640806f7a438427171141b91571a2e5886ef2
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-11 17:14:25 -0500

    debugging

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -134,7 +134,7 @@ aList_taper = buildTapering_symmetric(a,t,TN)
 print(aList_taper) # debugging
 for i in aList_taper:
     taper_box = BoxStructure(Vec3(0), Vec3(i,w0,h0), DielectricMaterial(2.6, order=2, color="red"))
-    taper_hole = CylinderStructure(Vec3(0), h0, d*i, DielectricMaterial(1, order=1, color="blue"))
+    taper_hole = CylinderStructure(Vec3(0), h0, d*i/2, DielectricMaterial(1, order=1, color="blue"))
     taper_cells += [UnitCell(structures=[ taper_box, taper_hole ], size=Vec3(i), engine=engine)]
 ############################################################################################################
 
