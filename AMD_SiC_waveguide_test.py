f151d8bac4fd250e3d2ca2d32e8ea38dfa0a2960
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-13 11:57:14 -0400

    debugging quasipotential sim

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -33,7 +33,7 @@ w = 1.75
 #taper prefactor (for the defect region)
 t = 0.84
 #taper prefactor (for the waveguide region)
-t_wvg = 0.7
+t_wvg = 0.8
 #beam height (set by epi-layer thickness)
 h0 = 250e-9
 # cavity beam length
