3541de521edf3cc09df3a65e94ef8f466304680e
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-13 13:53:50 -0400

    debugging quasipotential sim

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -101,8 +101,8 @@ r1["freq"], r1["vmode"],
 1/(1/r1["qxmin"] + 1/r1["qxmax"]),
 1/(2/r1["qymax"] + 1/r1["qzmin"] + 1/r1["qzmax"])
 ))
-# r1["xyprofile"].show()
-# r1["yzprofile"].show()
+r1["xyprofile"].show()
+r1["yzprofile"].show()
 
 cavity = Cavity1D(load_path="cavity_testing.obj",engine=engine)
 Qwvg = 1/(1/r1["qxmin"] + 1/r1["qxmax"])
@@ -117,7 +117,7 @@ print("Q: %f, P: %f" % ( Q, P))
 r1 = cavity.get_results("resonance")[-1]
 print(r1['res']["xyprofile"].max_loc())
 print(r1['res']["yzprofile"].max_loc())
-# r1["sess_res"].show()
+r1["sess_res"].show()
 # # #####################################################################################
 
 # # # evaluate the quasipotential
