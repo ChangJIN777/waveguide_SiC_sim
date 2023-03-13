e86385f651481ce219a93be09fb2acea7909cdc8
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-12 21:34:31 -0400

    debugging

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -32,7 +32,7 @@ w = 1.75
 #taper prefactor (for the defect region)
 t = 0.84
 #taper prefactor (for the waveguide region)
-t_wvg = 0.5
+t_wvg = 0.84
 #beam height (set by epi-layer thickness)
 h0 = 250e-9
 # cavity beam length
