ad459eeb7670f98fa5f57763ce60c9eff794356c
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-13 12:16:38 -0400

    debugging quasipotential sim

--- AMD_SiC_waveguide_test.py
+++ AMD_SiC_waveguide_test.py
@@ -48,8 +48,8 @@ prefactor_mirror_R = 0.9
 n_f = 2.6
 # Use level 4 automeshing accuracy, and show the Lumerical GUI while running simulations 
 FDTDloc="/n/sw/lumerical-2021-R2-2717-7bf43e7149_seas/"
-# engine = LumericalEngine(mesh_accuracy=5, hide=False, lumerical_path=FDTDloc, working_path="./fsps")
-engine = LumericalEngine(mesh_accuracy=5, hide=True, lumerical_path=FDTDloc, save_fsp=False)
+engine = LumericalEngine(mesh_accuracy=5, hide=False, lumerical_path=FDTDloc, working_path="./fsps")
+# engine = LumericalEngine(mesh_accuracy=5, hide=True, lumerical_path=FDTDloc, save_fsp=False)
 #the minimum lattice constant in the tapering region
 amin = a*t
     
