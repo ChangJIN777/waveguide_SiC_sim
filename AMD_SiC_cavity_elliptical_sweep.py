8bfea2a12ff31b49b01cc31cd62730fe0485681e
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-16 11:40:37 -0400

    sweeping lattice constant

--- AMD_SiC_cavity_elliptical_sweep.py
+++ AMD_SiC_cavity_elliptical_sweep.py
@@ -85,7 +85,7 @@ def run_Sim(param):
 
     #define mesh size (use 12nm for accuracy, currently set to 12nm)
     # man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 12e-9, dy=None, dz=None)
-    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 12e-9, dy=None, dz=None)
+    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 20e-9, dy=None, dz=None)
 
     # simulating the resonance and the Q #########################################################
     # r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
@@ -129,12 +129,13 @@ def run_Sim(param):
     record_data(data,file_name)
     
     end_time = datetime.now()
+    print('Duration: %f'%(fitness))
     print('Duration: {}'.format(end_time - start_time))
     
     return -1*fitness
 
 p0 = [2.912e-07]
-# popt = scipy.optimize.minimize(run_Sim,p0,method='Nelder-Mead')
+popt = scipy.optimize.minimize(run_Sim,p0,method='Nelder-Mead')
 
 # debugging 
-run_Sim(p0)
+# run_Sim(p0)
