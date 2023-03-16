1312313a090f9c56bcb27db94c48a5338ff30bad
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-16 17:12:27 -0400

    debugging the unit cell

--- AMD_SiC_unitCell_elliptical_Optimize.py
+++ AMD_SiC_unitCell_elliptical_Optimize.py
@@ -42,63 +42,15 @@ n_f = 2.6
 #the minimum lattice constant in the waveguide region 
 amin_wvg = t_wvg*a
 
+# optimizing for the mirror unit cells (SWEEPING CODE) ###################
+p0 = [a,d1,d2]
+bnd = ((2.0e-07,3.0e-07),(0.1,0.8),(0.67,w))
+popt = scipy.optimize.minimize(unitCellOptimization_SiC_elliptical,p0,method='Nelder-Mead')
 
-# here we try to simulate the field profile of a unit cell of the given dimension 
-# build the unit cell 
-unit_cell = buildUnitCell_elliptical(a,d1,d2)
-# decrease the beam length 
-l = a*2
-
-cavity = Cavity1D(
-unit_cells=  unit_cell,
-structures=[ BoxStructure(Vec3(0), Vec3(l, w*a, h0), DielectricMaterial(n_f, order=2, color="red")) ],
-center_shift=0,
-engine=engine
-)
-# By setting the save path here, the cavity will save itself after each simulation to this file
-cavity.save("cavity_elliptical.obj")
-
-#define mesh size (use 12nm for accuracy, currently set to 12nm)
-# man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 12e-9, dy=None, dz=None)
-man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 20e-9, dy=None, dz=None)
-
-# simulating the resonance and the Q #########################################################
-r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
-                    analyze_time=1000e-15,mesh_regions = [man_mesh], sim_size=Vec3(4,8,8))
-
-# Print the reults and plot the electric field profiles
-print("F: %f, Vmode: %f, Qwvg: %f, Qsc: %f" % (
-    r1["freq"], r1["vmode"],
-    1/(1/r1["qxmin"] + 1/r1["qxmax"]),
-    1/(2/r1["qymax"] + 1/r1["qzmin"] + 1/r1["qzmax"])
-))
-r1["xyprofile"].show()
-r1["yzprofile"].show()
-
-cavity = Cavity1D(load_path="cavity_testing.obj",engine=engine)
-Qwvg = 1/(1/r1["qxmin"] + 1/r1["qxmax"])
-Qsc = 1/(2/r1["qymax"] + 1/r1["qzmin"] + 1/r1["qzmax"])
-Vmode = r1["vmode"]
-F = r1["freq"]
-
-Q = 1/((1/Qsc) + (1/Qwvg))
-P = (Q*Qsc) / (Vmode*Vmode)
-print("Q: %f, P: %f" % ( Q, P))
-
-r1 = cavity.get_results("resonance")[-1]
-print(r1['res']["xyprofile"].max_loc())
-print(r1['res']["yzprofile"].max_loc())
-r1["sess_res"].show()
-
-# # optimizing for the mirror unit cells (SWEEPING CODE) ###################
-# p0 = [a,d1,d2]
-# bnd = ((2.0e-07,3.0e-07),(0.1,0.8),(0.67,w))
-# popt = scipy.optimize.minimize(unitCellOptimization_SiC_elliptical,p0,method='Nelder-Mead')
-
-# # optimizing for the waveguide unit cells
-# p0 = [amin_wvg]
-# bnd = ((0.5*a,a))
-# popt = scipy.optimize.minimize(unitCellOptimization_SiC_waveguide_elliptical,p0,method='Nelder-Mead')
+# optimizing for the waveguide unit cells
+p0 = [amin_wvg]
+bnd = ((0.5*a,a))
+popt = scipy.optimize.minimize(unitCellOptimization_SiC_waveguide_elliptical,p0,method='Nelder-Mead')
 
 
 # testing the algorithm
