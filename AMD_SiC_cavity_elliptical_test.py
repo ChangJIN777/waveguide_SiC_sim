fb4e8ffcb5aa766ff4e359750a942f19d206eb71
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-19 01:10:37 -0400

    testing the cavity

--- AMD_SiC_cavity_elliptical_test.py
+++ AMD_SiC_cavity_elliptical_test.py
@@ -55,7 +55,8 @@ MN_R = 3
 #the number of taper unit cells 
 TN = 8
 #set the center of the device
-centerCell = MN_L+TN-1 
+# centerCell = MN_L+TN-1 
+centerCell = MN_L+TN
 
 #build the left mirror cell region 
 mirror_cells_left = buildMirrorRegion_elliptical(a,hx,hy,MN_L,w0,h0,n_f,engine)
@@ -80,7 +81,7 @@ cavity.save("cavity_elliptical.obj")
 
 #define mesh size (use 12nm for accuracy, currently set to 12nm)
 # man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 12e-9, dy=None, dz=None)
-man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 20e-9, dy=None, dz=None)
+man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 12e-9, dy=None, dz=None)
 
 # simulating the resonance and the Q #########################################################
 # r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
