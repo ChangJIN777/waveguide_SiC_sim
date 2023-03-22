a60f54e1e5224b8c4330f16467548d279671cd7f
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-22 17:40:54 -0400

    increased the mesh size

--- AMD_SiC_cavity_elliptical_test.py
+++ AMD_SiC_cavity_elliptical_test.py
@@ -26,7 +26,7 @@ w = 1.75
 w0 = 1.75*a
 # w0 = 4.699560661981287e-7
 #taper prefactor (for the defect region)
-t = 0.869462767219858
+t = 0.8
 #beam height (set by epi-layer thickness)
 h0 = 250e-9
 # cavity beam length
