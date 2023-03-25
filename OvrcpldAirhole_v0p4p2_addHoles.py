c6e07f0c896614b15a7e50500d50779d0895e844
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-25 15:54:57 -0400

    testing erik's code

--- OvrcpldAirhole_v0p4p2_addHoles.py
+++ OvrcpldAirhole_v0p4p2_addHoles.py
@@ -19,9 +19,9 @@ FDTDexeLoc = os.path.join(FDTDLoc,'bin/fdtd-solutions')
 FDTDmpiLoc = os.path.join(FDTDLoc,'bin/fdtd-engine-ompi-lcl')
 
 
-hole_radii = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/holeStruct_v2.txt'), dtype=float, usecols=(0,1), unpack=False)
+hole_radii = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/holeStruct_v1.txt'), dtype=float, usecols=(0,1), unpack=False)
 hole_radii /= 2
-lattice_constants = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/periodStruct_v2.txt'), dtype=float, usecols=(0), unpack=False)
+lattice_constants = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/periodStruct_v1.txt'), dtype=float, usecols=(0), unpack=False)
 center_cell = 12
 # Unit cells are triangular prisms
 beam_width = 0.482e-6
