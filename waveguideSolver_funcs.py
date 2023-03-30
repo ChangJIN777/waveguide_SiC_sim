beb5504c6f7eba533b5399b30f92cd414e5a0d25
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-30 14:48:31 -0400

    sweeping for optimal hy

--- waveguideSolver_funcs.py
+++ waveguideSolver_funcs.py
@@ -1070,8 +1070,7 @@ def sweep_cellHeight_ellipticalCavity(a,hx,hx_min,hy,hy_min,t_wvg,WN,w0=w0,h0=h0
     # simulating the resonance and the Q #########################################################
     # r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
     #                     analyze_time=1000e-15,analyze_fspan=5.0e12,mesh_regions = [man_mesh], sim_size=Vec3(4,8,8))
-    r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
-                        analyze_time=1000e-15,mesh_regions = [man_mesh], sim_size=Vec3(4,4,8))
+    r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, analyze_time=1000e-15,mesh_regions = [man_mesh], sim_size=Vec3(4,4,8))
 
     # Print the reults and plot the electric field profiles
     print("F: %f, Vmode: %f, Qwvg: %f, Qsc: %f" % (
