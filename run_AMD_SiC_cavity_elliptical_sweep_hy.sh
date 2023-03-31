d56bcd82b5e5d3423ba4a5a1811f80e4fd7458f5
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-03-30 20:35:21 -0400

    sweeping hy

---
+++ run_AMD_SiC_cavity_elliptical_sweep_hy.sh
@@ -0,0 +1,6 @@
+module load Anaconda3 #Load Anaconda module
+source activate wv_sol_env # activate the wavesolver environment
+module load intel/21.2.0-fasrc01
+module load openmpi/4.1.1-fasrc01
+module load lumerical-seas/2021_7bf43e7149-fasrc01
+python ./AMD_SiC_cavity_elliptical_sweep_hy.py
