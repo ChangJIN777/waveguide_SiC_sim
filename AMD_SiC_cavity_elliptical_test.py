56777699570757fdc0dc7d12640ebe29e28d77c8
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-04-02 13:48:47 -0400

    testing the cavity

--- AMD_SiC_cavity_elliptical_test.py
+++ AMD_SiC_cavity_elliptical_test.py
@@ -96,7 +96,7 @@ cavity.save("cavity.obj")
 #define mesh size (use 12nm for accuracy, currently set to 12nm)
 # man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 12e-9, dy=None, dz=None)
 # man_mesh = MeshRegion(BBox(Vec3(0),Vec3(12e-6,0.7e-6,0.4e-6)), 20e-9, dy=None, dz=None)
-man_mesh = MeshRegion(BBox(Vec3(0),Vec3(10e-6,2e-6,2e-6)), 15e-9, dy=None, dz=None)
+man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,2e-6,2e-6)), 15e-9, dy=None, dz=None)
 
 # simulating the resonance and the Q #########################################################
 r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
