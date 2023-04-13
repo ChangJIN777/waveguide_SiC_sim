e9a3faf8fbd8532f15b31581b356eead66bd7172
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-04-13 16:06:01 -0400

    sweeping for overcoupled cavity v3

--- AMD_SiC_cavity_elliptical_sweep_v4.py
+++ AMD_SiC_cavity_elliptical_sweep_v4.py
@@ -98,7 +98,7 @@ def run_Sim(param):
 
     #define mesh size (use 12nm for accuracy, currently set to 12nm)
     # man_mesh = MeshRegion(BBox(Vec3(0),Vec3(4e-6,0.6e-6,0.5e-6)), 12e-9, dy=None, dz=None)
-    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(5e-6,2e-6,2e-6)), 12e-9, dy=None, dz=None)
+    man_mesh = MeshRegion(BBox(Vec3(0),Vec3(5e-6,2e-6,2e-6)), 15e-9, dy=None, dz=None)
 
     # simulating the resonance and the Q #########################################################
     # r1 = cavity.simulate("resonance", target_freq=target_frequency, source_pulselength=200e-15, 
