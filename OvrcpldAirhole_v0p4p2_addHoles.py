be28bf79fa0403c80b56065f91d2ac385478645a
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-04-13 18:26:51 -0400

    generating the overcoupled cavity benchmark

--- OvrcpldAirhole_v0p4p2_addHoles.py
+++ OvrcpldAirhole_v0p4p2_addHoles.py
@@ -20,6 +20,7 @@ FDTDmpiLoc = os.path.join(FDTDLoc,'bin/fdtd-engine-ompi-lcl')
 
 
 hole_radii = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/holeStruct.txt'), dtype=float, usecols=(0,1), unpack=False)
+hole_radii /= 2
 lattice_constants = np.loadtxt(os.path.join(os.path.curdir, 'v0p4p2/periodStruct.txt'), dtype=float, usecols=(0), unpack=False)
 center_cell = 12
 # Unit cells are triangular prisms
