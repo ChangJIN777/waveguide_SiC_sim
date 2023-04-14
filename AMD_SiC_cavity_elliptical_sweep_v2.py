b7acc34984fbee4e76e1f59054d8ac40850080ee
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-04-14 12:51:51 -0400

    sweeping cavity v7

--- AMD_SiC_cavity_elliptical_sweep_v2.py
+++ AMD_SiC_cavity_elliptical_sweep_v2.py
@@ -53,15 +53,15 @@ hymin_wvg = d_min*hy
 #the left mirror cell number 
 MN_L = 10 
 #the right mirror cell number 
-MN_R = 2
+MN_R = 1
 #the number of taper unit cells 
 TN = 5
-TN_L = 8
-TN_R = 4
+TN_L = 6
+TN_R = 6
 #set the center of the device
 centerCell = MN_L+TN_L-1
 #the number of cells in the waveguide region
-WN = 5
+WN = 3
 
 def run_Sim(param):
     print("Start sim ==============================")
@@ -155,7 +155,7 @@ def run_Sim(param):
     
     # record the data 
     data = [a,hx,hy,t,w0,prefactor_mirror_R,Vmode,Qwvg,Qsc,Qxmin,Qxmax,Q,F,detuning_wavelength,fitness]
-    file_name = "OptimizeListFull_Overcoupled_cavity_v4_sweep_t2.csv"
+    file_name = "OptimizeListFull_Overcoupled_cavity_v7_sweep_t1.csv"
     record_data(data,file_name)
     
     end_time = datetime.now()
