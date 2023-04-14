f5f082a0f1a87047d66dacadb0b8edc33ad70708
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-04-14 01:30:49 -0400

    sweeping cavity v5 trial 2

--- AMD_SiC_cavity_elliptical_sweep_v4.py
+++ AMD_SiC_cavity_elliptical_sweep_v4.py
@@ -15,13 +15,13 @@ from datetime import datetime
 from waveguideSolver_funcs import *
 
 #lattice constant
-a = 2.796929535831381e-07
+a = 284.3e-09
 #hole diameter in the x direction 
-hx = 7.046230850833163e-08
+hx = 72.40e-09
 #hole diameter in the y direction 
-hy = 1.240769835794245e-07
+hy = 127.5e-09
 #beam width prefactor
-w0 = 4.709362810532443e-07
+w0 = 468.6e-09
 #taper prefactor (for the defect region)
 t = 0.818
 #taper prefactor (for the waveguide region)
@@ -59,7 +59,7 @@ TN = 5
 TN_L = 8
 TN_R = 4
 #set the center of the device
-centerCell = MN_L+TN_L
+centerCell = MN_L+TN_L-1
 #the number of cells in the waveguide region
 WN = 5
 
@@ -140,7 +140,6 @@ def run_Sim(param):
     if Vmode < 0.48:
         Vmode = 1e6
 
-        
     P = (Q*Qsc)/ (Vmode*Vmode)
     print("Q: %f, P: %f, detuning: %f nm" % ( Q, P, detuning_wavelength_nm))
 
@@ -150,7 +149,7 @@ def run_Sim(param):
     
     # record the data 
     data = [a,hx,hy,t,w0,prefactor_mirror_R,Vmode,Qwvg,Qsc,Qxmin,Qxmax,Q,F,detuning_wavelength,fitness]
-    file_name = "OptimizeListFull_Overcoupled_cavity_v5_sweep_t1.csv"
+    file_name = "OptimizeListFull_Overcoupled_cavity_v5_sweep_t2.csv"
     record_data(data,file_name)
     
     end_time = datetime.now()
