2cec540b9e025d97811be374718cf7b08047d51b
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-05-05 14:02:34 -0400

    making the critically coupled cavity

---
+++ run_SiC_GDS_gen_v6.sh
@@ -0,0 +1,6 @@
+module load Anaconda3 #Load Anaconda module
+source activate wv_sol_env # activate the wavesolver environment
+module load intel/21.2.0-fasrc01
+module load openmpi/4.1.1-fasrc01
+module load lumerical-seas/2021_7bf43e7149-fasrc01
+python ./SiC_GDS_gen_v6.py
