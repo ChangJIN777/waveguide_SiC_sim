b5bb23f4f914faca8e33fe0ece0fb7513d7d2b32
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-08-11 19:06:27 -0700

    debugging the cluster

--- run_AMD_SiC_cavity_elliptical_test.sh
+++ run_AMD_SiC_cavity_elliptical_test.sh
@@ -1,5 +1,5 @@
-module load Anaconda3 #Load Anaconda module
-source activate wv_sol_env # activate the wavesolver environment
+module load python/3.10.9-fasrc01 #Load python module
+mamba activate wv_sol_env # activate the wavesolver environment
 module load intel/21.2.0-fasrc01
 module load openmpi/4.1.1-fasrc01
 module load lumerical-seas/2021_7bf43e7149-fasrc01
