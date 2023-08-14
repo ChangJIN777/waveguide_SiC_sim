8ee9ec169847fa420857a0221ed12ccd39eb2cc8
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-08-13 23:34:45 -0700

    sweeping the right mirror number

--- run_AMD_SiC_cavity_elliptical_optimization_v1.sh
+++ run_AMD_SiC_cavity_elliptical_optimization_v1.sh
@@ -1,5 +1,5 @@
 module load python/3.10.9-fasrc01 #Load python module
 mamba activate wv_sol_env # activate the wavesolver environment
 module load intel/23.0.0-fasrc01 
-module load lumerical-seas/2021_7bf43e7149-fasrc01
+module load lumerical-seas/2023R1_3_aa78b69550-fasrc01
 python ./AMD_SiC_cavity_elliptical_optimization_v1.py
