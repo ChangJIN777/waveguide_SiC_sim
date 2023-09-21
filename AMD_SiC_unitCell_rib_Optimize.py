99eecf859e35e8a01f822f9322ae4c20ed37021a
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-09-21 13:10:45 -0400

    optimizing the rib unit cells

--- AMD_SiC_unitCell_rib_Optimize.py
+++ AMD_SiC_unitCell_rib_Optimize.py
@@ -50,9 +50,9 @@ rib_sim_params["hide_GUI"] = True
 
 # # sweep the dimensions of the rib unit cell 
 rib_sim_params["simulationData_fileName"] = "SiC_500nm_rib_unitcell_testSweep_t6.txt"
-a = 2.666666666666666e-7
-a_min = a*0.95
-a_max = a*1.15
+a = 2.651851851851851e-07
+a_min = a*0.995
+a_max = a*1.005
 hx_min = 0.5e-7
 hx_max = 2e-7
 hy_min = 4e-7
--- sim_data/unitCell_param_Sweeps_analysis.m
+++ sim_data/unitCell_param_Sweeps_analysis.m
@@ -2,7 +2,7 @@
 % solver 
 clear; clc;
 %% import and store the data from the txt file 
-data = importdata("SiC_500nm_rib_unitcell_testSweep_t5.txt");
+data = importdata("SiC_500nm_rib_unitcell_testSweep_t6.txt");
 dielectric_band = data(:,6);
 air_band = data(:,7);
 mg = data(:,5);
