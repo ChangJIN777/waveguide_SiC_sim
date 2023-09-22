4a954dba37200b33ba72b26283078a6feac9c658
Chang Jin <49243865+ChangJIN777@users.noreply.github.com>
2023-09-21 21:54:03 -0400

    testing the new cavity

--- AMD_SiC_cavity_rib_test.py
+++ AMD_SiC_cavity_rib_test.py
@@ -23,11 +23,11 @@ rib_sim_params["save_fsps"] = True
 rib_sim_params["hide_GUI"] = False
 # testing the side coupling code 
 rib_cavity_params["do_sc"] = False
-rib_sim_params["running_cluster"] = True
+rib_sim_params["running_cluster"] = True 
 rib_sim_params["running_local"] = False
 
 # improve the mesh resolution
-rib_sim_params["mesh_res"] = 15e-9
+rib_sim_params["mesh_res"] = 12e-9
 rib_cavity_params["a"] = 2.659218106995883e-07
 rib_sim_params["boundary_condition"] = ['ymin']
 rib_cavity_params["C_lattice_tapering_prefactor"] = 0.85
