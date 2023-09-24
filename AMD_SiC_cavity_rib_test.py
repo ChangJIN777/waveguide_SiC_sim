041223ba6124865507700cfdf53c8ced37431f72
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-09-24 13:42:44 -0400

    testing the rib cavities

--- AMD_SiC_cavity_rib_test.py
+++ AMD_SiC_cavity_rib_test.py
@@ -30,10 +30,10 @@ rib_sim_params["running_local"] = False
 # improve the mesh resolution
 rib_sim_params["mesh_res"] = 12e-9
 # rib_cavity_params["a"] = 2.836499314128942e-07 # for the TE mode 
-rib_cavity_params["a"] = 2.6e-7 # for the TM mode 
+rib_cavity_params["a"] = 2.600124371284864e-07 # for the TM mode 
 rib_sim_params["boundary_condition"] = ['ymin','zmin']
 rib_sim_params["mode"] = 'TM'
-rib_cavity_params["C_lattice_tapering_prefactor"] = 0.7
+rib_cavity_params["C_lattice_tapering_prefactor"] = 0.7833333333333333
 rib_sim_params["freq_span"] = 3e14 
 rib_sim_params["simulationData_fileName"] = "SiC_500nm_debugging_cavity.csv"
 
