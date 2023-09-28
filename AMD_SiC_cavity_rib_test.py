017ff95ce338534eca4d1c10ecae94c2cb0be569
ChangJIN777 <49243865+ChangJIN777@users.noreply.github.com>
2023-09-28 00:23:42 -0400

    testing the rib cavity design

--- AMD_SiC_cavity_rib_test.py
+++ AMD_SiC_cavity_rib_test.py
@@ -30,7 +30,7 @@ rib_sim_params["running_local"] = False
 # improve the mesh resolution
 rib_sim_params["mesh_res"] = 12e-9
 # rib_cavity_params["a"] = 2.836499314128942e-07 # for the TE mode 
-rib_cavity_params["a"] = 3.e-07 # for the TM mode 
+rib_cavity_params["a"] = 2.85e-07 # for the TM mode 
 rib_cavity_params["hx"] = 1.8e-07 # for the TM mode 
 rib_cavity_params["hy"] = 3.84e-07 # for the TM mode 
 rib_cavity_params["beam_width"] = 4e-07 # for the TM mode 
